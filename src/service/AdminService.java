c02d45e725d51012e8b60b5e60378431b23ede9f
Samandar2201 <komilovsamandar54@gmail.com>
2025-05-20 17:31:26 +0500

    Add Student1

--- src/service/AdminService.java
+++ src/service/AdminService.java
@@ -1,5 +1,22 @@
 package service;
 
-public class AdminService {
+import entity.User;
+import entity.enums.Role;
 
+import static tools.Util.*;
+public class AdminService {
+    public void service(){
+        addStudent();
+    }
+
+    private void addStudent() {
+        User user=new User();
+        System.out.println("Enter fullName:");
+        user.setFullName(Strscanner.nextLine());
+        System.out.println("Enter email:");
+        user.setEmail(Strscanner.nextLine());
+        System.out.println("Enter password");
+        user.setPassword(Strscanner.nextLine());
+        user.setRole(Role.STUDENT);
+    }
 }
