09a7a5b76f1209da16856103cc6d040ab002f82d
bekhruz <hayitovbehruz27@gmail.com>
2025-05-20 16:40:43 +0500

    changed1

--- src/entity/Group.java
+++ src/entity/Group.java
@@ -10,4 +10,63 @@ public class Group {
     private User teacher;
     private Set<User> students;
 
+    public Group(){}
+    public Group(String id, String name, Integer maxLessonInMonth, User teacher, Set<User> students) {
+        this.id = id;
+        this.name = name;
+        this.maxLessonInMonth = maxLessonInMonth;
+        this.teacher = teacher;
+        this.students = students;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getMaxLessonInMonth() {
+        return maxLessonInMonth;
+    }
+
+    public void setMaxLessonInMonth(Integer maxLessonInMonth) {
+        this.maxLessonInMonth = maxLessonInMonth;
+    }
+
+    public User getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(User teacher) {
+        this.teacher = teacher;
+    }
+
+    public Set<User> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<User> students) {
+        this.students = students;
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", maxLessonInMonth=" + maxLessonInMonth +
+                ", teacher=" + teacher +
+                ", students=" + students +
+                '}';
+    }
 }
