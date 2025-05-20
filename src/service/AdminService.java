247072b34123d785e13fb81ce71cfcf50330ec47
khondamir <xondamirmusaxonov212@gmail.com>
2025-05-20 18:09:30 +0500

    new pulls

--- src/service/AdminService.java
+++ src/service/AdminService.java
@@ -22,6 +22,7 @@ public class AdminService {
                 }
                 case "2"->{}
                 case "3"->{
+                    editTeacher();
                 }
                 case "4"->{}
                 case "5"->{}
