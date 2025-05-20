c41700856f77fd2abbf98e314e4fa0194cda02f4
omonjon123 <odilmahkamov62@gmail.com>
2025-05-20 17:30:33 +0500

    Omonjon edit student1

--- src/service/AdminService.java
+++ src/service/AdminService.java
@@ -1,5 +1,18 @@
 package service;
-
+import static tools.Util.*;
+import static db.Datacourse.*;
 public class AdminService {
+    public void service(){
+        editStudent();
+    }
+
+    private void editStudent() {
+        for (int i = 0; i < ; i++) {
+            
+        }
+        System.out.println("Enter Student id");
+        String id = Strscanner.nextLine();
+        
+    }
 
 }
